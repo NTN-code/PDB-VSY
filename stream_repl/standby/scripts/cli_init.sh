ca20ec93a0c7adb198ca791de127625254f3ebcd
NTN-code <92088758+AntonTkacouITechArt@users.noreply.github.com>
2022-11-28 14:10:33 +0300

    fix_stand_by

--- stream_repl/standby/scripts/cli_init.sh
+++ stream_repl/standby/scripts/cli_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-chown postgres:postgres ~/.pgpass
-chmod 0600 ~/.pgpass
+# chown postgres:postgres ~/.pgpass
+# chmod 0600 ~/.pgpass
 
 pg_basebackup --host=172.28.0.2 --username=repluser --pgdata=/var/lib/postgresql/repl --wal-method=stream --write-recovery-conf 
